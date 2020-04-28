4f84b0d0d6332c4eae79dc6691cb288d27b470fd
Raghav Sai <g.raghav.sai@gmail.com>
2020-04-28 17:18:55 -0400

    Setup Apollo Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const httpLink = createHttpLink({
+	uri: process.env.API_SERVER_URL,
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<ApolloProvider client={client}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</ApolloProvider>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
