ba17d1891bb015e3fa92a61afe4261a7c68416ab
Raghav Sai <g.raghav.sai@gmail.com>
2020-04-30 13:34:14 -0400

    Updated feed

--- src/components/ChirpList/ChirpList.tsx
+++ src/components/ChirpList/ChirpList.tsx
@@ -1,14 +1,44 @@
+import "./ChirpList.css";
+
 import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import { FeedQuery, Chirp } from "../../generated/graphql";
 
 import ChirpCard from "../ChirpCard/ChirpCard";
 
-import "./ChirpList.css";
+const FEED_QUERY = gql`
+	query feed {
+		feed {
+			id
+			content
+			createdAt
+			author {
+				id
+				name
+			}
+		}
+	}
+`;
 
 export default function ChirpList() {
+	const { loading, error, data } = useQuery<FeedQuery>(FEED_QUERY);
+
+	if (loading) {
+		return <div className="chirp-list flex-column">loading chirps...</div>;
+	}
+
+	if (error || !data) {
+		return (
+			<div className="chirp-list flex-column">error getting chirp feed :(</div>
+		);
+	}
+
 	return (
 		<div className="chirp-list flex-column">
-			This is the Chirp List.
-			<ChirpCard />
+			{data.feed?.map((chirp: Chirp) => (
+				<ChirpCard chirp={chirp} />
+			))}
 		</div>
 	);
 }
