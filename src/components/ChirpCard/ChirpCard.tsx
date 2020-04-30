7c492c5a52c35551344ce2b0d6cf3cd36d2986a9
Raghav Sai <g.raghav.sai@gmail.com>
2020-04-30 13:29:54 -0400

    Updated chirp card styles

--- src/components/ChirpCard/ChirpCard.tsx
+++ src/components/ChirpCard/ChirpCard.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import "./ChirpCard.css";
 
-export default function ChirpCard() {
-	return <div className="chirp-card">Chirp</div>;
+import React from "react";
+import { Chirp } from "../../generated/graphql";
+
+type ChirpCardProps = {
+	chirp: Chirp;
+};
+
+export default function ChirpCard({ chirp }: ChirpCardProps) {
+	return (
+		<div className="card flex-column">
+			<div className="chirp-card__content">{chirp.content}</div>
+			<div className="chirp-card__meta">Metadata</div>
+		</div>
+	);
 }
