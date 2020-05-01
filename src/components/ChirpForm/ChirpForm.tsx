ac3dbbb9541561f0670c07d666da5adb32ceb1e7
Raghav Sai <g.raghav.sai@gmail.com>
2020-04-30 21:57:33 -0400

    Revamp CSS and use variables

--- src/components/ChirpForm/ChirpForm.tsx
+++ src/components/ChirpForm/ChirpForm.tsx
@@ -4,8 +4,13 @@ import "./ChirpForm.css";
 
 export default function ChirpForm() {
 	return (
-		<div className="chirp-form">
-			<textarea className="chirp-input"></textarea>
+		<div className="chirp-form flex-column">
+			<textarea
+				className="chirp-form__textarea card"
+				placeholder="what's buggin' ya?"
+				aria-label="Chirp Text"
+			/>
+			<button className="chirp-form__button card" type="submit">chirp!</button>
 		</div>
 	);
 }
