3c8958dadf09211768c7a7db5ab10717392abd6e
Raghav Sai <g.raghav.sai@gmail.com>
2020-05-05 14:19:42 -0400

    Setup login form and updated CSS

--- src/components/LoginRegisterModal/LoginRegisterModal.tsx
+++ src/components/LoginRegisterModal/LoginRegisterModal.tsx
@@ -2,6 +2,7 @@ import "./LoginRegisterModal.css";
 
 import React from "react";
 import ReactModal from "react-modal";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 type propTypes = {
 	showModal: boolean;
@@ -12,22 +13,47 @@ export default function LoginRegisterModal({
 	showModal,
 	setShowModal,
 }: propTypes) {
+	const handleLogin = () => {};
+
 	return (
 		<ReactModal
 			isOpen={showModal}
-			className="login-register-modal__content"
+			className="login-register-modal__content card card--secondary"
 			overlayClassName="login-register-modal__overlay"
-			contentLabel={"Login/Register Modal"}
+			contentLabel="Login/Register Modal"
 			onRequestClose={(e) => setShowModal(false)}
 			appElement={document.getElementById("root")!}
 		>
 			<button
-				className="button card login-register-modal__button"
+				className="button card login-register-modal__close"
 				onClick={(e) => setShowModal(false)}
 			>
 				X
 			</button>
-			<p>test</p>
+			<Formik
+				initialValues={{ username: "", password: "" }}
+				onSubmit={handleLogin}
+			>
+				<Form className="flex-column">
+					<Field
+						name="username"
+						type="text"
+						className="card login-register-modal__input"
+						placeHolder="username"
+						aria-label="username"
+					/>
+					<ErrorMessage name="username" />
+					<Field
+						name="password"
+						type="password"
+						className="card login-register-modal__input"
+						placeHolder="password"
+						aria-label="password"
+					/>
+					<ErrorMessage name="password" />
+					<button className="button card">login</button>
+				</Form>
+			</Formik>
 		</ReactModal>
 	);
 }
