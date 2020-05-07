1aff1d99579780dc94f2acdea2691c1be2bdcc9f
Raghav Sai <g.raghav.sai@gmail.com>
2020-05-07 17:51:25 -0400

    Toggle chirp form based on auth context

--- src/components/ChirpForm/ChirpForm.tsx
+++ src/components/ChirpForm/ChirpForm.tsx
@@ -1,18 +1,28 @@
 import "./ChirpForm.css";
 
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function ChirpForm() {
+	const { authenticated } = useContext(AuthContext);
+	const [auth, setAuth] = useState(false);
+
+	useEffect(() => {
+		setAuth(authenticated);
+	}, [authenticated])
+
 	return (
-		<div className="chirp-form flex-column">
-			<textarea
-				className="chirp-form__textarea card"
-				placeholder="what's buggin' ya?"
-				aria-label="Chirp Text"
-			/>
-			<button className="button card" type="submit">
-				chirp!
-			</button>
-		</div>
+		auth ? (
+			<div className="chirp-form flex-column">
+				<textarea
+					className="chirp-form__textarea card"
+					placeholder="what's buggin' ya?"
+					aria-label="Chirp Text"
+				/>
+				<button className="button card" type="submit">
+					chirp!
+				</button>
+			</div>
+		) : null
 	);
 }
