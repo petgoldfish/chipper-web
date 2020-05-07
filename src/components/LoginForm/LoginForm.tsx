816667d1247561a114a8523674b614645d4a55c6
Raghav Sai <g.raghav.sai@gmail.com>
2020-05-07 15:51:36 -0400

    Refactored Login form

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,100 @@
+import "./LoginForm.css";
+
+import React, { ReactElement } from "react";
+import gql from "graphql-tag";
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import { object, string } from "yup";
+import { useMutation } from "@apollo/react-hooks";
+import { LoginMutation } from "../../generated/graphql";
+import { setAuthToken } from "../../authToken";
+
+interface Props {
+	setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface LoginFormValues {
+	username: string;
+	password: string;
+}
+
+const LOGIN_MUTATION = gql`
+	mutation login($username: String!, $password: String!) {
+		login(name: $username, password: $password) {
+			token
+			userId
+		}
+	}
+`;
+
+export default function LoginForm({ setShowModal }: Props): ReactElement {
+	const [login, { error }] = useMutation<LoginMutation>(LOGIN_MUTATION);
+	const initialValues: LoginFormValues = { username: "", password: "" };
+
+	const handleLogin = async (
+		{ username, password }: LoginFormValues,
+		actions: FormikHelpers<LoginFormValues>
+	) => {
+		actions.resetForm();
+		actions.setSubmitting(true);
+		const { data } = await login({ variables: { username, password } });
+		setAuthToken(data?.login.token);
+		actions.setSubmitting(false);
+		setShowModal(false);
+	};
+
+	const loginValidationSchema = object().shape({
+		username: string().required("username is required"),
+		password: string().required("password is required"),
+	});
+
+	return (
+		<Formik
+			initialValues={initialValues}
+			validationSchema={loginValidationSchema}
+			onSubmit={handleLogin}
+		>
+			{({ isSubmitting }) => (
+				<Form className="flex-column">
+					<Field
+						name="username"
+						type="text"
+						className="card login-form__input"
+						placeholder="username"
+						aria-label="username"
+					/>
+					<ErrorMessage name="username">
+						{(error) => (
+							<div className="login-form__error">{error}</div>
+						)}
+					</ErrorMessage>
+					<Field
+						name="password"
+						type="password"
+						className="card login-form__input"
+						placeholder="password"
+						aria-label="password"
+					/>
+					<ErrorMessage name="password">
+						{(error) => (
+							<div className="login-form__error">{error}</div>
+						)}
+					</ErrorMessage>
+					<button disabled={isSubmitting} className="button card" type="submit">
+						login
+					</button>
+					{error &&
+						error.graphQLErrors.map((gqlError, index) => (
+							<div key={index} className="login-form__error">
+								{gqlError.message}
+							</div>
+						))}
+					{error && error.networkError && (
+						<div className="login-form__error">
+							error connecting to server :(
+						</div>
+					)}
+				</Form>
+			)}
+		</Formik>
+	);
+}
--- src/components/LoginRegisterModal/LoginRegisterModal.tsx
+++ src/components/LoginRegisterModal/LoginRegisterModal.tsx
@@ -1,57 +1,16 @@
-import "./LoginRegisterModal.css";
-
 import React from "react";
 import ReactModal from "react-modal";
-import gql from "graphql-tag";
-import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
-import { object, string } from "yup";
-import { useMutation } from "@apollo/react-hooks";
-import { LoginMutation } from "../../generated/graphql";
-import { setAuthToken } from "../../authToken";
+import LoginForm from "../LoginForm/LoginForm";
 
 type PropTypes = {
 	showModal: boolean;
 	setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-type LoginFormValues = {
-	username: string;
-	password: string;
-};
-
-const LOGIN_MUTATION = gql`
-	mutation login($username: String!, $password: String!) {
-		login(name: $username, password: $password) {
-			token
-			userId
-		}
-	}
-`;
-
 export default function LoginRegisterModal({
 	showModal,
 	setShowModal,
 }: PropTypes) {
-	const [login, { error }] = useMutation<LoginMutation>(LOGIN_MUTATION);
-	const initialValues: LoginFormValues = { username: "", password: "" };
-
-	const handleLogin = async (
-		{ username, password }: LoginFormValues,
-		actions: FormikHelpers<LoginFormValues>
-	) => {
-		actions.resetForm();
-		actions.setSubmitting(true);
-		const { data } = await login({ variables: { username, password } });
-		setAuthToken(data?.login.token);
-		actions.setSubmitting(false);
-		setShowModal(false);
-	};
-
-	const loginValidationSchema = object().shape({
-		username: string().required("username is required"),
-		password: string().required("password is required"),
-	});
-
 	return (
 		<ReactModal
 			isOpen={showModal}
@@ -83,58 +42,7 @@ export default function LoginRegisterModal({
 			>
 				X
 			</button>
-			<Formik
-				initialValues={initialValues}
-				validationSchema={loginValidationSchema}
-				onSubmit={handleLogin}
-			>
-				{({ isSubmitting }) => (
-					<Form className="flex-column">
-						<Field
-							name="username"
-							type="text"
-							className="card login-register-modal__input"
-							placeholder="username"
-							aria-label="username"
-						/>
-						<ErrorMessage name="username">
-							{(error) => (
-								<div className="login-register-modal__error">{error}</div>
-							)}
-						</ErrorMessage>
-						<Field
-							name="password"
-							type="password"
-							className="card login-register-modal__input"
-							placeholder="password"
-							aria-label="password"
-						/>
-						<ErrorMessage name="password">
-							{(error) => (
-								<div className="login-register-modal__error">{error}</div>
-							)}
-						</ErrorMessage>
-						<button
-							disabled={isSubmitting}
-							className="button card"
-							type="submit"
-						>
-							login
-						</button>
-						{error &&
-							error.graphQLErrors.map((gqlError, index) => (
-								<div key={index} className="login-register-modal__error">
-									{gqlError.message}
-								</div>
-							))}
-						{error && error.networkError && (
-							<div className="login-register-modal__error">
-								error connecting to server :(
-							</div>
-						)}
-					</Form>
-				)}
-			</Formik>
+			<LoginForm setShowModal={setShowModal} />
 		</ReactModal>
 	);
 }
