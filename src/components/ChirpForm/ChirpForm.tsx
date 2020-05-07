e17c646646980a23d1e00ab6429b3bf8c898d4eb
Raghav Sai <g.raghav.sai@gmail.com>
2020-05-07 18:10:01 -0400

    Implemented logout

--- src/components/ChirpForm/ChirpForm.tsx
+++ src/components/ChirpForm/ChirpForm.tsx
@@ -1,18 +1,13 @@
 import "./ChirpForm.css";
 
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 export default function ChirpForm() {
 	const { authenticated } = useContext(AuthContext);
-	const [auth, setAuth] = useState(false);
-
-	useEffect(() => {
-		setAuth(authenticated);
-	}, [authenticated])
 
 	return (
-		auth ? (
+		authenticated ? (
 			<div className="chirp-form flex-column">
 				<textarea
 					className="chirp-form__textarea card"
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,37 @@
 import "./Header.css";
 
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import LoginRegisterModal from "../LoginRegisterModal/LoginRegisterModal";
+import { AuthContext } from "../../context/AuthContext";
+import { setAuthToken } from "../../authToken";
 
 export default function Header() {
 	const [showModal, setShowModal] = useState(false);
+	const { authenticated, setAuthenticated } = useContext(AuthContext);
+
+	function logout() {
+		setAuthToken(null);
+		setAuthenticated(false);
+	}
 
 	return (
 		<div className="chipper__header">
 			<h1 className="chipper__header__title">chipper</h1>
-			<button
-				className="chipper__header__login button card"
-				onClick={(e) => setShowModal(true)}
-			>
-				login
-			</button>
+			{authenticated ? (
+				<button
+					className="chipper__header__login button card"
+					onClick={logout}
+				>
+					logout
+				</button>
+			) : (
+				<button
+					className="chipper__header__login button card"
+					onClick={(e) => setShowModal(true)}
+				>
+					login
+				</button>
+			)}
 			<LoginRegisterModal showModal={showModal} setShowModal={setShowModal} />
 		</div>
 	);
