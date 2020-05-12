21debb9ed720829d3c65e82b8e2a16437202b67f
Raghav Sai <g.raghav.sai@gmail.com>
2020-05-12 17:49:35 -0400

    Authenticated the Me route

--- src/components/Me/Me.tsx
+++ src/components/Me/Me.tsx
@@ -1,8 +1,15 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useContext } from "react";
 import { RouteComponentProps } from "@reach/router";
+import { AuthContext } from "../../context/AuthContext";
 
 interface Props {}
 
 export default function Me(props: Props & RouteComponentProps): ReactElement {
-	return <div>Me</div>;
+	const { authenticated } = useContext(AuthContext);
+
+	return authenticated ? (
+		<div>Me</div>
+	) : (
+		<div>you must be authenticated to view this page</div>
+	);
 }
